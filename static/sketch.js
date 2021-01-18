8ec8c1a46dcd9d37787ea713da4622ce9d7915ae
conoha <xxx>
2021-01-18 23:45:36 +0900

    topにmargin

--- static/sketch.js
+++ static/sketch.js
@@ -1,4 +1,4 @@
-y=80
+y=0
 setup=_=>{
   createCanvas(w=320,w)
   h=w/2
@@ -29,15 +29,41 @@ f=(x,y,n=4)=>
     f(cos(3*x)-sin(4*y),x*x-y*y-2*x*y,n-1)
     :(x-y)**2*24
 
+/*
+y=0
+setup=_=>
+  createCanvas(w=320,h=180)
+draw=_=>{
+  if(y<h){
+    for(x=w;x--;point(x,y)){
+      stroke(l=f(x/h-.9,y/h-.5))
+      if(check(x,y)){
+        if(l>0){
+          stroke(l)
+        }else{
+          stroke(0)
+        }
+      }else{
+        stroke(255-l)
+      }
+    }
+    y++
+  }
+}
+f=(x,y,c=0)=>x*x+y*y>4?c:f(x*y*2-2/3,sin(y*y-x*x),c+2)
+
+*/
+
+
 check=(x,y)=>{
   judge = false;
   dot = 20;
+  y-=100
   xx=int(x/dot)
-  y-=80
   if(x%dot==0 || x%dot==19 || y%dot==0 || y%dot==19){
     judge = false;
   }
-  else if(int(y/dot)==0){
+  else if(y > 0 && int(y/dot)==0){
     if(xx==0 || xx==1 || xx==2 || xx==5){
       judge = true;
     }
@@ -69,3 +95,4 @@ check=(x,y)=>{
   }
   return judge;
 }
+
